09a07861d8e31e0f74c1192ea2eaa268cbcf0703
Diagg <lacournicolas200@msn.com>
2022-05-18 20:34:10 +0200

    Version: 0.15.1

--- Applications-Data/AdobeAcrobatReaderDC-Command.ps1
+++ Applications-Data/AdobeAcrobatReaderDC-Command.ps1
@@ -1,11 +1,5 @@
 # Install
-#Powershell.exe -executionpolicy bypass -command "$P=$($env:temp+'\'+(New-guid)+'.Ps1');(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Diagg/EverGreen-Application-Installer/master/EverGreen Apps Installer/BootStrap-EverGreenInstaller.ps1',$P);;Iex ""$P -Application AdobeAcrobatReaderDC -Language French -Architecture x86 -SetAsDefault"""
+#Powershell.exe -executionpolicy bypass -command "$P=$($env:temp+'\'+(New-guid)+'.Ps1');(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Diagg/Greenstaller/master/Greenstaller.ps1',$P);;Iex ""$P -Application AdobeAcrobatReaderDC -Language French -Architecture x86 -SetAsDefault"""
 
 #Uninstall
-#Powershell.exe -executionpolicy bypass -command "$P=$($env:temp+'\'+(New-guid)+'.Ps1');(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Diagg/EverGreen-Application-Installer/master/EverGreen Apps Installer/BootStrap-EverGreenInstaller.ps1',$P);Iex ""$P -Application AdobeAcrobatReaderDC -Uninstall"""
-
-#Stage without install
-#Powershell.exe -executionpolicy bypass -command "$P=$($env:temp+'\'+(New-guid)+'.Ps1');(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Diagg/EverGreen-Application-Installer/master/EverGreen Apps Installer/BootStrap-EverGreenInstaller.ps1',$P);Iex ""$P -Application AdobeAcrobatReaderDC -PreDownloadPath 'C:\temp' -Language French -Architecture x86"""
-
-#Install staged content
-#Powershell.exe -executionpolicy bypass -command "$P=$($env:temp+'\'+(New-guid)+'.Ps1');(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Diagg/EverGreen-Application-Installer/master/EverGreen Apps Installer/BootStrap-EverGreenInstaller.ps1',$P);Iex ""$P -Application AdobeAcrobatReaderDC -InstallSourcePath 'C:\temp\AdobeAcrobatReaderDC\App.exe' -SetAsDefault"""
+#Powershell.exe -executionpolicy bypass -command "$P=$($env:temp+'\'+(New-guid)+'.Ps1');(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Diagg/Greenstaller/master/Greenstaller.ps1',$P);Iex ""$P -Application AdobeAcrobatReaderDC -Uninstall"""
