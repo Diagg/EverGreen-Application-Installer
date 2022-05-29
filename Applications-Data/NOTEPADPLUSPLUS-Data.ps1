969d63e1f89aa6e5308b4a55d4250d60b12acef7
Diagg <lacournicolas200@msn.com>
2022-05-30 00:45:57 +0200

    Version 0.17.1

--- Applications-Data/NOTEPADPLUSPLUS-Data.ps1
+++ Applications-Data/NOTEPADPLUSPLUS-Data.ps1
@@ -1,4 +1,4 @@
-# Version 0.3 - 29/05/2022 
+# Version 0.4 - 30/05/2022 
 
 Function Get-AppInfo
     {
@@ -109,93 +109,116 @@ Function Get-AppUpdateStatus
 
 Function Invoke-AdditionalInstall
     {
-
-        # Set Default App Association
-        $Script_LogPath = "`$ContentPath = ""$($ECK.ContentPath)"" `n"
-
-        $Script_Assoc = {
-                ."$ContentPath\SFTA.ps1"
-                Set-PTA -ProgId AcroExch.Document.DC -Protocol .pdf
-                Set-PTA -ProgId AcroExch.Document.DC -Protocol .pdfxml
-            }
-
-        Write-ECKlog "Setting file association for $($Script:AppInfo.AppInstallName) !"
-        $ScriptBlock = [ScriptBlock]::Create($Script_LogPath.ToString() + $Script_Assoc.ToString())
-        Invoke-ECKScheduledTask -TaskName 'Set-Assoc' -Context user -ScriptBlock $ScriptBlock -now -WaitFinished
-
-
         If ($Script:AppInfo.AppInstallOptionEnterprise)
             {
-                # Remove Desktop Icon
-                If (test-path "C:\Users\Public\Desktop\Acrobat Reader DC.lnk")
-                    {
-                        Write-ECKlog "Removing desktop Icon for $($Script:AppInfo.AppInstallName) !"
-                        Remove-Item "C:\Users\Public\Desktop\Acrobat Reader DC.lnk" -Force -ErrorAction SilentlyContinue|Out-Null
-                    }
-
-
                 # Remove Automatic Updates
                 Write-ECKlog "Removing automatic update for $($Script:AppInfo.AppInstallName) !"
                 $Script:AppInfo.AppInstallOptionDisableUpdate = $true
-
-                # Remove Welcome message
-                Write-ECKlog "Disabling first tour welcome Popup"
-                if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWelcomeScreen") -ne $true) {New-Item "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWelcomeScreen" -force -ea SilentlyContinue|Out-Null }
-                New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWelcomeScreen' -Name 'bShowWelcomeScreen' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue|Out-Null
-
-                # Remove 'Try Adobe Pro DC' button
-                Write-ECKlog "Disabling advertising button"
-                if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown") -ne $true) {New-Item "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -force -ea SilentlyContinue|Out-Null }
-                New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown' -Name 'bAcroSuppressUpsell' -Value 1 -PropertyType DWord -Force -ea SilentlyContinue|Out-Null
-
-
-                $ScriptBlock_firstTour = {
-                        $Reg = "HKCU:\SOFTWARE\Adobe\Acrobat Reader\DC\FTEDialog"
-                        if((Test-Path -LiteralPath $Reg) -ne $true) {New-Item $Reg -force -ea SilentlyContinue }
-                        New-ItemProperty -LiteralPath $Reg -Name 'iFTEVersion' -Value 0x0000000a -PropertyType DWord -Force -ea SilentlyContinue
-                        New-ItemProperty -LiteralPath $Reg -Name 'iLastCardShown' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue
-                    }
-
-                Invoke-ECKScheduledTask -TaskName 'Disable-AdobeFirstTour' -Context user -ScriptBlock $ScriptBlock_firstTour -now -WaitFinished
-
             } 
     }
 
 
 Function Invoke-AdditionalUninstall
     {
-
-        # Restore Default App Association
-        $Script_LogPath = "`$ContentPath = ""$($ECK.ContentPath)"" `n"
-
-        $Script_Assoc = {
-                ."$ContentPath\SFTA.ps1"
-                Set-PTA -ProgId MSEdgeHTM -Protocol .pdf                
-            }
-
-        Write-ECKlog "Restoring default file association !"
-        $ScriptBlock = [ScriptBlock]::Create($Script_LogPath.ToString() + $Script_Assoc.ToString())
-        Invoke-ECKScheduledTask -TaskName 'Set-Assoc' -Context user -ScriptBlock $ScriptBlock -now -WaitFinished
-
-        Unregister-ScheduledTask -TaskName "Adobe Acrobat Update Task" -Confirm:$false -ErrorAction SilentlyContinue
-        If (Get-service "AdobeARMService" -ErrorAction SilentlyContinue){sc.exe delete "AdobeARMService"}
-
+        Write-ECKlog "Nothing set for Additional Uninstall option !"
     }
 
 
 Function Invoke-DisableUpdateCapability
     {
-        Write-ECKlog "Removing Adobe Scheduled task"
-        Unregister-ScheduledTask -TaskName "Adobe Acrobat Update Task" -Confirm:$false -ErrorAction SilentlyContinue
-        
-        If (Get-service "AdobeARMService" -ErrorAction SilentlyContinue)
+        Write-ECKlog "Disabling update capability for $($Script:AppInfo.AppInstallName)"
+
+        $ConfigXML = @"
+<?xml version="1.0" encoding="UTF-8" ?>
+<NotepadPlus>
+    <FindHistory nbMaxFindHistoryPath="10" nbMaxFindHistoryFilter="10" nbMaxFindHistoryFind="10" nbMaxFindHistoryReplace="10" matchWord="no" matchCase="no" wrap="yes" directionDown="yes" fifRecuisive="yes" fifInHiddenFolder="no" fifProjectPanel1="no" fifProjectPanel2="no" fifProjectPanel3="no" fifFilterFollowsDoc="no" fifFolderFollowsDoc="no" searchMode="0" transparencyMode="1" transparency="150" dotMatchesNewline="no" isSearch2ButtonsMode="no" regexBackward4PowerUser="no" />
+    <History nbMaxFile="10" inSubMenu="no" customLength="-1" />
+    <ProjectPanels>
+        <ProjectPanel id="0" workSpaceFile="" />
+        <ProjectPanel id="1" workSpaceFile="" />
+        <ProjectPanel id="2" workSpaceFile="" />
+    </ProjectPanels>
+    <GUIConfigs>
+        <GUIConfig name="ToolBar" visible="yes">standard</GUIConfig>
+        <GUIConfig name="StatusBar">show</GUIConfig>
+        <GUIConfig name="TabBar" dragAndDrop="yes" drawTopBar="yes" drawInactiveTab="yes" reduce="yes" closeButton="yes" doubleClick2Close="no" vertical="no" multiLine="no" hide="no" quitOnEmpty="no" iconSetNumber="0" />
+        <GUIConfig name="ScintillaViewsSplitter">vertical</GUIConfig>
+        <GUIConfig name="UserDefineDlg" position="undocked">hide</GUIConfig>
+        <GUIConfig name="TabSetting" replaceBySpace="no" size="4" />
+        <GUIConfig name="AppPosition" x="256" y="290" width="1100" height="700" isMaximized="no" />
+        <GUIConfig name="FindWindowPosition" left="0" top="0" right="0" bottom="0" />
+        <GUIConfig name="FinderConfig" wrappedLines="no" purgeBeforeEverySearch="no" />
+        <GUIConfig name="noUpdate" intervalDays="15" nextUpdateDate="20220613">yes</GUIConfig>
+        <GUIConfig name="Auto-detection">yes</GUIConfig>
+        <GUIConfig name="CheckHistoryFiles">no</GUIConfig>
+        <GUIConfig name="TrayIcon">no</GUIConfig>
+        <GUIConfig name="MaitainIndent">yes</GUIConfig>
+        <GUIConfig name="TagsMatchHighLight" TagAttrHighLight="yes" HighLightNonHtmlZone="no">yes</GUIConfig>
+        <GUIConfig name="RememberLastSession">yes</GUIConfig>
+        <GUIConfig name="DetectEncoding">yes</GUIConfig>
+        <GUIConfig name="SaveAllConfirm">yes</GUIConfig>
+        <GUIConfig name="NewDocDefaultSettings" format="0" encoding="4" lang="0" codepage="-1" openAnsiAsUTF8="yes" />
+        <GUIConfig name="langsExcluded" gr0="0" gr1="0" gr2="0" gr3="0" gr4="0" gr5="0" gr6="0" gr7="0" gr8="0" gr9="0" gr10="0" gr11="0" gr12="0" langMenuCompact="yes" />
+        <GUIConfig name="Print" lineNumber="yes" printOption="3" headerLeft="" headerMiddle="" headerRight="" footerLeft="" footerMiddle="" footerRight="" headerFontName="" headerFontStyle="0" headerFontSize="0" footerFontName="" footerFontStyle="0" footerFontSize="0" margeLeft="0" margeRight="0" margeTop="0" margeBottom="0" />
+        <GUIConfig name="Backup" action="0" useCustumDir="no" dir="" isSnapshotMode="yes" snapshotBackupTiming="7000" />
+        <GUIConfig name="TaskList">yes</GUIConfig>
+        <GUIConfig name="MRU">yes</GUIConfig>
+        <GUIConfig name="URL">2</GUIConfig>
+        <GUIConfig name="uriCustomizedSchemes">svn:// cvs:// git:// imap:// irc:// irc6:// ircs:// ldap:// ldaps:// news: telnet:// gopher:// ssh:// sftp:// smb:// skype: snmp:// spotify: steam:// sms: slack:// chrome:// bitcoin:</GUIConfig>
+        <GUIConfig name="globalOverride" fg="no" bg="no" font="no" fontSize="no" bold="no" italic="no" underline="no" />
+        <GUIConfig name="auto-completion" autoCAction="3" triggerFromNbChar="1" autoCIgnoreNumbers="yes" insertSelectedItemUseENTER="yes" insertSelectedItemUseTAB="yes" funcParams="yes" />
+        <GUIConfig name="auto-insert" parentheses="no" brackets="no" curlyBrackets="no" quotes="no" doubleQuotes="no" htmlXmlTag="no" />
+        <GUIConfig name="sessionExt"></GUIConfig>
+        <GUIConfig name="workspaceExt"></GUIConfig>
+        <GUIConfig name="MenuBar">show</GUIConfig>
+        <GUIConfig name="Caret" width="1" blinkRate="600" />
+        <GUIConfig name="ScintillaGlobalSettings" enableMultiSelection="no" />
+        <GUIConfig name="openSaveDir" value="0" defaultDirPath="" />
+        <GUIConfig name="titleBar" short="no" />
+        <GUIConfig name="stylerTheme" path="C:\Users\WDAGUtilityAccount\AppData\Roaming\Notepad++\stylers.xml" />
+        <GUIConfig name="insertDateTime" customizedFormat="yyyy-MM-dd HH:mm:ss" reverseDefaultOrder="no" />
+        <GUIConfig name="wordCharList" useDefault="yes" charsAdded="" />
+        <GUIConfig name="delimiterSelection" leftmostDelimiter="40" rightmostDelimiter="41" delimiterSelectionOnEntireDocument="no" />
+        <GUIConfig name="multiInst" setting="0" />
+        <GUIConfig name="MISC" fileSwitcherWithoutExtColumn="yes" fileSwitcherExtWidth="50" fileSwitcherWithoutPathColumn="yes" fileSwitcherPathWidth="50" backSlashIsEscapeCharacterForSql="yes" writeTechnologyEngine="0" isFolderDroppedOpenFiles="no" docPeekOnTab="no" docPeekOnMap="no" sortFunctionList="no" saveDlgExtFilterToAllTypes="no" muteSounds="no" />
+        <GUIConfig name="Searching" monospacedFontFindDlg="no" stopFillingFindField="no" findDlgAlwaysVisible="no" confirmReplaceInAllOpenDocs="yes" replaceStopsWithoutFindingNext="no" />
+        <GUIConfig name="searchEngine" searchEngineChoice="2" searchEngineCustom="" />
+        <GUIConfig name="MarkAll" matchCase="no" wholeWordOnly="yes" />
+        <GUIConfig name="SmartHighLight" matchCase="no" wholeWordOnly="yes" useFindSettings="no" onAnotherView="no">yes</GUIConfig>
+        <GUIConfig name="DarkMode" enable="no" colorTone="0" customColorTop="2105376" customColorMenuHotTrack="4210752" customColorActive="4210752" customColorMain="2105376" customColorError="176" customColorText="14737632" customColorDarkText="12632256" customColorDisabledText="8421504" customColorEdge="6579300" customColorLinkText="65535" />
+        <GUIConfig name="ScintillaPrimaryView" lineNumberMargin="show" lineNumberDynamicWidth="yes" bookMarkMargin="show" indentGuideLine="show" folderMarkStyle="box" lineWrapMethod="aligned" currentLineIndicator="1" currentLineFrameWidth="1" virtualSpace="no" scrollBeyondLastLine="yes" rightClickKeepsSelection="no" disableAdvancedScrolling="no" wrapSymbolShow="hide" Wrap="no" borderEdge="yes" isEdgeBgMode="no" edgeMultiColumnPos="" zoom="0" zoom2="0" whiteSpaceShow="hide" eolShow="hide" borderWidth="2" smoothFont="no" paddingLeft="0" paddingRight="0" distractionFreeDivPart="4" />
+        <GUIConfig name="DockingManager" leftWidth="200" rightWidth="200" topHeight="200" bottomHeight="200">
+            <ActiveTabs cont="0" activeTab="-1" />
+            <ActiveTabs cont="1" activeTab="-1" />
+            <ActiveTabs cont="2" activeTab="-1" />
+            <ActiveTabs cont="3" activeTab="-1" />
+        </GUIConfig>
+    </GUIConfigs>
+</NotepadPlus>
+"@
+
+        If ($ECK.OSArchitectureIsX64 -eq $true)
             {
-                Write-ECKlog "Removing Adobe service"
-                sc.exe delete "AdobeARMService"
+                $uPath = "${env:ProgramFiles(x86)}\Notepad++\config.model.xml"
+                $uFolder = "${env:ProgramFiles(x86)}\Notepad++\updater"
+            } 
+        Else 
+            {
+                $uPath = "$($env:ProgramFiles)\Notepad++\config.model.xml"
+                $uFolder = "$($env:ProgramFiles)\Notepad++\updater"
             }
-        
-        
-        Write-ECKlog "Hiding Search for update in menu"
-        if((Test-Path -LiteralPath "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown") -ne $true) {New-Item "HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -force -ea SilentlyContinue|Out-Null }
-        New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown' -Name 'bUpdater' -Value 0 -PropertyType DWord -Force -ea SilentlyContinue|Out-Null
+
+        $ConfigXML| Out-file -FilePath $uPath -Encoding UTF8 -Append -width 1000 -ErrorAction SilentlyContinue
+        If (test-path $uPath)
+            {Write-ECKlog "Disable update configuration applied succesfully !"}
+        else 
+            {Write-ECKlog "[ERROR] unable to apply disable update configuration !" -Type 3}
+
+
+        If (test-path $ufolder){Remove-Item -Path $uFolder  -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue}
+        If (-not (test-path $ufolder))
+            {Write-ECKlog "update engine removed succesfully !"}
+        else 
+            {Write-ECKlog "[ERROR] unable to remove update engine !" -Type 3}
+
     }
